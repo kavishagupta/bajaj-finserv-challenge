168c2080532065f0db944b79f2cb9247f93ff7f4
kavisha gupta <92855704+kavishagupta@users.noreply.github.com>
2024-09-22 17:39:55 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -70,4 +70,11 @@ export default {
     },
   },
   plugins: [require("tailwindcss-animate")],
+  module.exports = {
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
 };
